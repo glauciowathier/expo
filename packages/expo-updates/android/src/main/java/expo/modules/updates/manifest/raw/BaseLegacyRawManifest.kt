e5cbfd620ed0869f7e4e7157baffbab2859e4e1b
William Schurman <wschurman@gmail.com>
2021-04-13 13:16:20 -0700

    [updates] Add support for loading new manifests in Expo Go

--- packages/expo-updates/android/src/main/java/expo/modules/updates/manifest/raw/BaseLegacyRawManifest.kt
+++ packages/expo-updates/android/src/main/java/expo/modules/updates/manifest/raw/BaseLegacyRawManifest.kt
@@ -7,4 +7,16 @@ import org.json.JSONObject
 abstract class BaseLegacyRawManifest(json: JSONObject) : RawManifest(json) {
     fun getMetadata(): JSONObject? = json.optJSONObject("metadata")
     override fun getAssets(): JSONArray? = json.optJSONArray("assets")
+
+    @Throws(JSONException::class)
+    override fun getBundleURL(): String = json.getString("bundleUrl")
+
+    override fun getSDKVersionNullable(): String? = if (json.has("sdkVersion")) {
+        json.optString("sdkVersion")
+    } else {
+        null
+    }
+
+    @Throws(JSONException::class)
+    override fun getSDKVersion(): String = json.getString("sdkVersion")
 }
